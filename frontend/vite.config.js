62482dc8a3531216a0b9f14db17bc86507da214c
rupa singh <rupadivya12@gmail.com>
2026-05-02 01:05:01 +0530

    new chnag

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://team-task-manager-production-7115.up.railway.app',
         changeOrigin: true,
       },
     },
